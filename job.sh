2745a90acf03ec9e6aff818ad13a0c00e01af99d
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-02 20:22:16 +0530

    Update job.sh

--- job.sh
+++ job.sh
@@ -67,9 +67,12 @@ cd ./AWSCICD-Terraform/modules/jenkinsEC2
 terraform init
 terraform validate
 terraform fmt
-#terraform plan
-terraform apply -var 'AWS_ACCESS_KEY=${AWS_ACCESS_KEY}' -var 'AWS_SECRET_KEY=${AWS_SECRET_KEY}' -var 'REGION=${REGION}'
-yes
+
+# Plan the Terraform changes 
+echo "Planning Terraform changes..." 
+terraform plan -var "AWS_ACCESS_KEY=${AWS_ACCESS_KEY}" -var "AWS_SECRET_KEY=${AWS_SECRET_KEY}" -var "REGION=${REGION}" -out=tfplan
+terraform apply -auto-approve tfplan
+
 # Capture the output variable value
 JENKINS_MACHINE_IP=$(terraform output -raw instance_public_ip)
 # Use the captured value
