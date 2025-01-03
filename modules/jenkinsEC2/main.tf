55f1c309085ab9634d6635d618b46c3eb7e4eaba
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-03 21:18:39 +0530

    Update main.tf

--- modules/jenkinsEC2/main.tf
+++ modules/jenkinsEC2/main.tf
@@ -72,3 +72,22 @@ resource "aws_instance" "main1" {
         echo "Hello, World" > /var/www/html/index.html
         EOF
  }
+
+
+
+
+
+
+output "instance_id" {
+    description = "The id of ec2 instance"
+    value =  aws_instance.main1[0].id
+}
+
+output "instance_public_ip" {
+    description = "the publice ip address of ec2 instance"
+    value = aws_instance.main1[0].public_ip
+}
+
+output "instance_ip" {
+  value = length(aws_instance.main1) > 0 ? aws_instance.main1[0].public_ip : "No instance created"
+}
