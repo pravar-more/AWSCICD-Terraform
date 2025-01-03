2282407fc3fbec5ea6d9ff81977e04b7765ec0f9
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-03 20:17:05 +0530

    Update outputs.tf

--- modules/jenkinsEC2/outputs.tf
+++ modules/jenkinsEC2/outputs.tf
@@ -7,3 +7,7 @@ output "instance_public_ip" {
     description = "the publice ip address of ec2 instance"
     value = aws_instance.main[0].public_ip
 }
+
+output "instance_ip" {
+  value = length(aws_instance.main) > 0 ? aws_instance.main[0].public_ip : "No instance created"
+}
