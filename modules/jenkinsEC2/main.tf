4eed00eb325378b964a17ea57267562b22dd8283
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-03 22:27:16 +0530

    Update main.tf

--- modules/jenkinsEC2/main.tf
+++ modules/jenkinsEC2/main.tf
@@ -1,16 +1,16 @@
 # Check if security group exists 
-data "aws_security_group" "existing_sg" { 
-    
-    filter {
-        name = "group-name" 
-        values = [var.sg_name] 
-    }
-    #depends_on = [aws_security_group.main]
-}
+#data "aws_security_group" "existing_sg" { 
+#    
+#   filter {
+#        name = "group-name" 
+#        values = [var.sg_name] 
+#    }
+#    #depends_on = [aws_security_group.main]
+#}
 
 
 resource "aws_security_group" "main" {
-    count = length(data.aws_security_group.existing_sg.id) == 0 ? 1 : 0
+#    count = length(data.aws_security_group.existing_sg.id) == 0 ? 1 : 0
     name = var.sg_name
     
     ingress {
@@ -40,27 +40,27 @@ resource "aws_security_group" "main" {
     }
 }
 
-locals { 
-    sg_id = length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.main[0].id 
-}
+#locals { 
+#    sg_id = length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.main[0].id 
+#}
 
 # Check if instance exists 
-data "aws_instance" "existing_instance" {
-    filter { 
-        name = "tag:Name" 
-        values = [var.INSTANCE_NAME] 
-    } 
-    count = length(var.INSTANCE_NAME) > 0 ? 1 : 0
-}
+#data "aws_instance" "existing_instance" {
+#    filter { 
+#        name = "tag:Name" 
+#        values = [var.INSTANCE_NAME] 
+#    } 
+#    count = length(var.INSTANCE_NAME) > 0 ? 1 : 0
+#}
 
 resource "aws_instance" "main1" {
-    count = length(data.aws_instance.existing_instance.id) == 0 ? 1 : 0
+#   count = length(data.aws_instance.existing_instance.id) == 0 ? 1 : 0
     ami = lookup(var.AMI_MAP, var.REGION, "ami-005fc0f236362e99f")
 #   ami = "ami-005fc0f236362e99f"
     instance_type = var.INSTANCE_TYPE 
     key_name = var.KEY_NAME 
-    #security_groups = count.index == 0 ? [aws_security_group.main[0].id] : [data.aws_security_group.existing_sg[0].id]
-    security_groups = [local.sg_id]
+    security_groups = aws_security_group.main.id
+#    security_groups = [local.sg_id]
     tags = {
         Name = var.INSTANCE_NAME
     }
