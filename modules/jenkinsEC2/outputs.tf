64623416d774084617103f552189508d0e7fe075
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-03 20:08:28 +0530

    Update outputs.tf

--- modules/jenkinsEC2/outputs.tf
+++ modules/jenkinsEC2/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
     description = "The id of ec2 instance"
-    value =  aws_instance.main.id
+    value =  aws_instance.main[0].id
 }
 
 output "instance_public_ip" {
     description = "the publice ip address of ec2 instance"
-    value = aws_instance.main.public_ip
+    value = aws_instance.main[0].public_ip
 }
