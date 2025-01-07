6978c66fba662c83e2e498669124ded5c926c496
pravar-more <more.pravar@gmail.com>
2025-01-07 19:43:32 +0530

    ss

--- ansibleJob.sh
+++ ansibleJob.sh
@@ -3,11 +3,13 @@
 ###########################################################################
 #Ansible Playbook configurations
 #ansible-playbook <playbookFile.yaml>
-JENKINS_MACHINE_IP = $(cat /tmp/my_var_file)
-echo $(cat /tmp/my_var_file)
 echo $JENKINS_MACHINE_IP
+#JENKINS_MACHINE_IP = $(cat /tmp/my_var_file)
+echo $(cat /tmp/my_var_file)
 echo "in ansibleJob.sh"
-echo "[jenkinsMachine] $JENKINS_MACHINE_IP 
-      [eks] " > ./playbooks/inventory1.ini
+echo "[jenkinsMachine] 
+$JENKINS_MACHINE_IP 
+[eks] 
+" > ./playbooks/inventory1.ini
 
 #ansible-playbook jenkinsMachinePlaybook.yaml
