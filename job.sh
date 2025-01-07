35df8d55cc0463b53d26776bb8bd1423863c8e9b
pravar-more <more.pravar@gmail.com>
2025-01-07 18:24:01 +0530

    sss

--- job.sh
+++ job.sh
@@ -3,13 +3,17 @@
 echo "00000000000000ini Setup"
 chmod +x iniSetup.sh
 ./iniSetup.sh
+cd
 #############################
 #provisionResource Terraform
+cd ./AWSCICD-Terraform/
 echo "00000000000000proviosn resources"
 chmod +x provisionResource_job.sh
 source iniSetup.sh
 ./provisionResource_job.sh
+cd
 #############################
+cd ./AWSCICD-Terraform/
 echo "00000000000000ansible job"
 chmod +x ansibleJob.sh
 source provisionResource_job.sh
