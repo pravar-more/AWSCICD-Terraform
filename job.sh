97229d17c11c72db4e6edd975d27e047a62467db
pravar-more <more.pravar@gmail.com>
2025-01-07 16:25:43 +0530

    nw

--- job.sh
+++ job.sh
@@ -2,9 +2,11 @@ chmod +x iniSetup.sh
 #./iniSetup.sh
 #############################
 #provisionResource Terraform
-chmod +x provisionResource.sh
+echo "proviosn resources"
+chmod +x provisionResource_job.sh
 ./provisionResource_job.sh
 #############################
+echo "ansible job"
 chmod +x ansibleJob.sh
 ./ansibleJob.sh
 #############################
