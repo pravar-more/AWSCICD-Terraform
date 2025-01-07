c2ff959106ec47a79dd3c9ffbeaec941e5ae7e5f
pravar-more <more.pravar@gmail.com>
2025-01-07 20:05:56 +0530

    jj

--- ansibleJob.sh
+++ ansibleJob.sh
@@ -7,8 +7,9 @@ echo $JENKINS_MACHINE_IP
 #JENKINS_MACHINE_IP = $(cat /tmp/my_var_file)
 echo $(cat /tmp/my_var_file)
 echo "in ansibleJob.sh"
-echo "[jenkinsMachine] 
-$JENKINS_MACHINE_IP 
+echo "[ec2Server] 
+jenkinsMachine ansible_host=$JENKINS_MACHINE_IP
 [eks] 
 " > ./playbooks/inventory1.ini
+cd ./playbooks
 ansible-playbook jenkinsMachinePlaybook.yaml
