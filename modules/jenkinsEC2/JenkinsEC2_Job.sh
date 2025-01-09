acc3a1d5401ff03c5e7f778d94232f1716b8d8eb
pravar-more <more.pravar@gmail.com>
2025-01-09 19:47:43 +0530

    fwsf

--- modules/jenkinsEC2/JenkinsEC2_Job.sh
+++ modules/jenkinsEC2/JenkinsEC2_Job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
     
     sleep 80
-    ch $HOME/AWSCICD-Terraform/jobs/
+    cd $HOME/AWSCICD-Terraform/jobs/
     chmod +x ssh-keygenJob.sh
     ./ssh-keygenJob.sh
     echo "Hello, World" > /var/www/html/index.html
