ae6d58f2b9fc49a12dc6cfa38ba9df191220cd81
pravar-more <more.pravar@gmail.com>
2025-01-09 18:27:23 +0530

    sd

--- jobs/provisionResource_job.sh
+++ jobs/provisionResource_job.sh
@@ -33,7 +33,7 @@ terraform fmt
 
 # Plan the Terraform changes 
 echo "Planning Terraform changes..." 
-terraform plan -var "AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID" -var "AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY" -var "REGION=$AWS_DEFAULT_REGION" -out=tfplan
+terraform plan -var="AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID" -var="AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY" -var="REGION=$AWS_DEFAULT_REGION" -out=tfplan
 terraform apply -auto-approve tfplan
 
 # Capture the output variable value
